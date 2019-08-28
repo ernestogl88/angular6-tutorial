bce1bacd88d3efb902a1c5f86ac32d05d0ee5ac5
Ernesto <ernesto@bdeo.es>
2019-08-28 15:12:34 +0200

    Creado componente product detail

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {products} from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -6,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-
+  products = products;
   constructor() { }
 
   ngOnInit() {
